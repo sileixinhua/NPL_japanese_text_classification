48892d1473480ad2625d4540e516291e80cdcce9
silei <sileiruby@163.com>
2017-05-17 05:21:09 +0800

    爬取yahoo新闻并处理

--- src/WebSpiderYahoo.java
+++ src/WebSpiderYahoo.java
@@ -81,42 +81,40 @@ public class WebSpiderYahoo {
     }
 
     public static void main(String[] args) {
-        String web_site_key="6239974";
-        String web_site_url="https://news.yahoo.co.jp/pickup/"+web_site_key;
-        try{
-            URL u = new URL(web_site_url);
-            URLConnection connection = u.openConnection();
-            HttpURLConnection htCon = (HttpURLConnection) connection;
-            int code = htCon.getResponseCode();
-            if (code == HttpURLConnection.HTTP_OK)
-            {
-                System.out.println("找到网页地址。。。");
-                BufferedReader in=new BufferedReader(new InputStreamReader(htCon.getInputStream()));
-                String inputLine;
-                System.out.println("正在读取网页文本内容。。。");
-                System.out.println("网页文本内容写入"+"./data/yahoo/"+web_site_key+".txt"+"。。。");
-                while ((inputLine = in.readLine()) != null) {
-                    inputLine=delHTMLTag(inputLine);
-                    inputLine=stripHtml(inputLine);
-                    inputLine=inputLine.replaceAll("\\d+","");
-                    inputLine=inputLine.replaceAll("[a-zA-Z]","" );
-                    //System.out.println(inputLine);
-                    writeIntoFile(inputLine,"./data/yahoo/"+web_site_key+".txt", true);
+        int web_site_key_int = 6229974;
+        for (;web_site_key_int<=6249974;web_site_key_int++) {
+            String web_site_key = String.valueOf(web_site_key_int);
+            String web_site_url = "https://news.yahoo.co.jp/pickup/" + web_site_key;
+            try {
+                URL u = new URL(web_site_url);
+                URLConnection connection = u.openConnection();
+                HttpURLConnection htCon = (HttpURLConnection) connection;
+                int code = htCon.getResponseCode();
+                if (code == HttpURLConnection.HTTP_OK) {
+                    System.out.println("找到网页地址。。。");
+                    BufferedReader in = new BufferedReader(new InputStreamReader(htCon.getInputStream()));
+                    String inputLine;
+                    System.out.println("正在读取网页文本内容。。。");
+                    System.out.println("网页文本内容写入" + "./data/yahoo/" + web_site_key + ".txt" + "。。。");
+                    while ((inputLine = in.readLine()) != null) {
+                        inputLine = delHTMLTag(inputLine);
+                        inputLine = stripHtml(inputLine);
+                        inputLine = inputLine.replaceAll("\\d+", "");
+                        inputLine = inputLine.replaceAll("[a-zA-Z]", "");
+                        inputLine = inputLine.replaceAll("(\\r\\n|\\r|\\n|\\n\\r)", "");
+                        inputLine = inputLine.replaceAll("\\s", "");
+                        //System.out.println(inputLine);
+                        writeIntoFile(inputLine,"./data/yahoo/"+web_site_key+".txt", true);
+                    }
+                    in.close();
+                } else {
+                    System.out.println("Can not access the website");
                 }
-                in.close();
+            } catch (MalformedURLException e) {
+                System.out.println("Wrong URL");
+            } catch (IOException e) {
+                System.out.println("Can not connect");
             }
-            else
-            {
-                System.out.println("Can not access the website");
-            }
-        }
-        catch(MalformedURLException e )
-        {
-            System.out.println("Wrong URL");
-        }
-        catch(IOException e)
-        {
-            System.out.println("Can not connect");
         }
     }
 }
